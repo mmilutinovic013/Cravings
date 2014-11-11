a7f3e5fa46fe165eae0f88ce309ea1cb52e41159
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2014-11-11 16:56:08 -0500

    Updated FoodList

--- src/FoodList.java
+++ src/FoodList.java
@@ -1,22 +1,21 @@
-
-import java.util.ArrayList;
-
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-/**
- *
- * @author markymark1346
- */
+import java.util.ArrayList;
+
 public class FoodList {
-    private ArrayList<Food> theFoodList = new ArrayList<Food>();
+    private static ArrayList<Food> theListOfFoods = new ArrayList();
+    
     public FoodList(){
-        
+        for (int i = 0; i < 100; i++) {
+            
+        }
     }
     
-    public ArrayList<Food> getListOfFoods(){
-        return theFoodList;
+    public static ArrayList<Food> getListOfFoods(){
+        return theListOfFoods;
     }
 }
